bdfc6796b45017b3e7d35be5c2447642ae32cab9
Donkey <pig199881@qq.com>
2022-09-23 00:48:40 +0800

    chore: remove useless dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? './' : '/todolist',
+  base: command === 'serve' ? './' : '/todolist/',
   plugins: [vueJsx()],
 }))
