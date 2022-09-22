01323e87b7c652e6f3cf550824560ce9a022940a
Donkey <pig199881@qq.com>
2022-09-23 00:44:26 +0800

    build: change production base dir

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? './' : '/todolist',
   plugins: [vueJsx()],
-})
+}))
